802c28c5fab61510679086babc9987509040929c
jdbonilla5-droid <jdbonilla5@espe.edu.ec>
2025-12-15 19:10:48 +0000

    implementacion de la  funcion mostrar tabla con filtros y busqueda

--- public/js/main.js
+++ public/js/main.js
@@ -43,3 +43,95 @@ function agregarProducto() {
 
     mostrarTabla();
 }
+// Implementar funcion mostrar tabla con filtros y busqueda
+
+// Evento Change - Filtro
+$('#select_filtro').change(function() {
+    mostrarTabla();
+});
+
+// Evento Keyup - Buscar
+$('#txt_buscar').keyup(function() {
+    mostrarTabla();
+});
+
+// Evento Change - Ordenar
+$('#select_orden').change(function() {
+    mostrarTabla();
+});
+
+// Funcion mostrar tabla
+function mostrarTabla() {
+    
+    var filtro = $('#select_filtro').val();
+    var buscar = $('#txt_buscar').val().toLowerCase();
+    var orden = $('#select_orden').val();
+
+    var lista = [];
+    var i, j;
+
+    // Filtrar productos
+    for (i = 0; i < productos.length; i++) {
+        
+        var agregar = true;
+
+        if (filtro !== 'Todos' && productos[i].categoria !== filtro) {
+            agregar = false;
+        }
+
+        if (buscar !== '' && productos[i].nombre.toLowerCase().indexOf(buscar) === -1) {
+            agregar = false;
+        }
+
+        if (agregar) {
+            lista.push(productos[i]);
+        }
+    }
+
+    // Ordenar ascendente
+    if (orden === 'asc') {
+        for (i = 0; i < lista.length - 1; i++) {
+            for (j = 0; j < lista.length - i - 1; j++) {
+                if (lista[j].precio > lista[j + 1].precio) {
+                    var temp = lista[j];
+                    lista[j] = lista[j + 1];
+                    lista[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Ordenar descendente
+    if (orden === 'desc') {
+        for (i = 0; i < lista.length - 1; i++) {
+            for (j = 0; j < lista.length - i - 1; j++) {
+                if (lista[j].precio < lista[j + 1].precio) {
+                    var temp = lista[j];
+                    lista[j] = lista[j + 1];
+                    lista[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Limpiar tabla
+    $('#tabla_productos').empty();
+
+    if (lista.length === 0) {
+        $('#tabla_productos').html('<tr><td colspan="5" class="text-center">No hay productos</td></tr>');
+        return;
+    }
+
+    // Mostrar productos
+    for (i = 0; i < lista.length; i++) {
+        var fila = '<tr>' +
+                        '<td>' + lista[i].id + '</td>' +
+                        '<td>' + lista[i].nombre + '</td>' +
+                        '<td>$' + lista[i].precio.toFixed(2) + '</td>' +
+                        '<td>' + lista[i].categoria + '</td>' +
+                        '<td><button class="btn btn-danger btn-sm btn_eliminar" data-id="' + lista[i].id + '">Eliminar</button></td>' +
+                   '</tr>';
+        
+        $('#tabla_productos').append(fila);
+    }
+}
